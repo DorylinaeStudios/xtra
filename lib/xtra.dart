09e2580aadbb169447e84a994e8b8ab7325d886a
Jonas Lindberg <eksno@protonmail.com>
2022-11-10 10:27:43 +0100

    Create dart package

---
+++ lib/xtra.dart
@@ -0,0 +1,8 @@
+/// Package containing useful data types
+///
+/// More dartdocs go here.
+library xtra;
+
+export 'src/xtra_base.dart';
+
+// TODO: Export any libraries intended for clients of this package.
